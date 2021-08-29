54c950a579fe3b05033373af63e6e16f3b3df3b4
Abdullahi Odesanmi <56407386+realabdullah@users.noreply.github.com>
2021-08-29 19:57:37 +0100

    functionalities

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@
           </a>
         </li>
 
-  <hr>
+        <hr>
 
         <li>
           <a href="#">
@@ -39,6 +39,42 @@
           </a>
         </li>
       </ul>
+    </div>
+  </div>
+  <div class="desktop-nav">
+    <div class="nav-one">
+      <img src="./assets/images/logo.svg" alt="logo">
+      <nav>
+        <li>
+          <a href="#">
+            Features
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            Pricing
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            Resources
+          </a>
+        </li>
+      </nav>
+    </div>
+    <div class="nav-two">
+      <nav>
+         <li>
+          <a href="#">
+            Login
+          </a>
+        </li>
+        <li class="sign">
+          <a href="#">
+            Sign Up
+          </a>
+        </li>
+      </nav>
     </div>
   </div>
   <div class="hero-section">
@@ -55,9 +91,24 @@
   </div>
   <div class="main">
     <div class="short-it">
-      <input type="text" placeholder="Shorten a link here...">
+      <input type="text" v-model="oldLink" placeholder="Shorten a link here...">
+      <button @click="shortLink" class="short-btn">
+        <p v-if="!gettingLink">Shorten It!</p>
+        <img src="./assets/images/load.svg" alt="load" v-else>
+      </button>
+    </div>
+    <div class="shorted">
+      <p class="origin-url">
+        {{ oldLink }}
+      </p>
+      <p class="short-url">
+        {{ newLink }}
+      </p>
       <button class="short-btn">
-        Shorten It!
+        Copy
+      </button>
+      <button class="copy-btn">
+        Copied!
       </button>
     </div>
     <div class="about">
@@ -71,13 +122,13 @@
         <p>Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instil confidence in your content.</p>
       </div>
       <div class="stroke"></div>
-      <div class="feature">
+      <div class="feature one">
         <img src="./assets/images/icon-detailed-records.svg" alt="detailed-records">
         <h2>Detailed Records</h2>
         <p>Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.</p>
       </div>
       <div class="stroke"></div>
-      <div class="feature">
+      <div class="feature two">
         <img src="./assets/images/icon-fully-customizable.svg" alt="fully-customizable">
         <h2>Fully Customizable</h2>
         <p>Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.</p>
@@ -176,10 +227,15 @@
 
 <script>
 import { ref } from 'vue'
+import axios from 'axios'
 
 export default {
   setup() {
     const modal = ref(false)
+    const oldLink = ref('')
+    const linkData = ref('')
+    const newLink = ref('')
+    const gettingLink = ref(false)
 
     const modalOption = () => {
       if(modal.value == false) {
@@ -189,10 +245,31 @@ export default {
         modal.value = false
         console.log('hidden')
       }
-      
+    }
+
+    const shortLink = async () => {
+      gettingLink.value = true
+      const api_url = 'https://api.shrtco.de/v2/shorten?url='
+      const givenLink = oldLink.value
+      const url = api_url + givenLink
+      axios.post(url)
+      .then(response => {
+        gettingLink.value = false
+        linkData.value = response.data
+        newLink.value = linkData.value.result.short_link
+        console.log(newLink)
+        document.querySelector('.shorted').style.display = 'block'
+      })
+      .catch(error => {
+        console.log(error)
+      })
     }
     
     return {
+      oldLink,
+      shortLink,
+      gettingLink,
+      newLink,
       modal,
       modalOption
     }
@@ -208,6 +285,10 @@ export default {
   padding: 0;
   box-sizing: border-box;
   font-family: 'Poppins', sans-serif;
+}
+
+.desktop-nav {
+  display: none;
 }
 
 .header {
@@ -349,6 +430,58 @@ hr {
   font-weight: 700;
 }
 
+.main .shorted {
+  margin-top: 70px;
+  width: 100%;
+  padding: 20px;
+  border-radius: 10px;
+  background-color: hsl(0, 0%, 100%);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  display: none;
+}
+
+.main .shorted .origin-url {
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 10px;
+  border-bottom: 1px solid grey;
+}
+
+.main .shorted .short-url {
+  width: 100%;
+  padding: 10px;
+  color: hsl(180, 66%, 49%);
+}
+
+.main .shorted .short-btn {
+  width: 100%;
+  color: #fff;
+  background-color: hsl(180, 66%, 49%);
+  padding: 15px;
+  border: none;
+  border-radius: 10px;
+  font-weight: 700;
+}
+
+.short-btn img {
+  width: 24px;
+}
+
+.main .shorted .copy-btn {
+  display: none;
+  width: 100%;
+  color: #fff;
+  background-color: hsl(255, 11%, 22%);
+  padding: 15px;
+  border: none;
+  border-radius: 10px;
+  font-weight: 700;
+}
+
 .about {
   text-align: center;
   margin: 100px 0;
@@ -420,6 +553,7 @@ hr {
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  text-align: center;
 }
 
 .cta-section h1 {
@@ -514,5 +648,201 @@ hr {
 
 .socials li {
   padding: 10px;
+}
+
+@media(min-width: 900px) {
+  .header-box {
+    display: none;
+  }
+
+  .desktop-nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 40px;
+  }
+
+  .nav-one {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .nav-one img {
+    margin-right: 10px;
+  }
+
+  .nav-one nav {
+    list-style: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .nav-one nav li {
+    padding: 0 15px;
+  }
+
+  .nav-one nav li a  {
+    text-decoration: none;
+    color: hsl(257, 7%, 63%);
+    font-weight: 700;
+  }
+
+  .nav-two nav {
+    list-style: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .nav-two nav li {
+    padding: 0 15px;
+  }
+
+  .nav-two nav li a {
+    text-decoration: none;
+    color: hsl(257, 7%, 63%);
+    font-weight: 700;
+  }
+
+  .nav-two nav .sign {
+    background-color: hsl(180, 66%, 49%);
+    padding: 10px 20px;
+    border: none;
+    border-radius: 50px;
+    font-weight: 700;
+  }
+
+  .nav-two nav .sign a {
+    color: #fff;
+  }
+
+  .hero-section {
+    flex-direction: row-reverse;
+    justify-content: space-between;
+  }
+
+  .hero-image {
+    width: 50%;
+  }
+
+  .hero-image img {
+    width: 500px;
+  }
+
+  .hero-text {
+    margin-top: 0px;
+    text-align: left;
+    margin-left: 80px;
+    margin-right: 60px;
+  }
+
+  .main .short-it {
+    top: -50px;
+    padding: 30px;
+    background-image: url('./assets/images/bg-shorten-desktop.svg');
+    background-position: 100px;
+    flex-direction: row;
+  }
+
+  .main .short-it input {
+    padding: 10px;
+    margin-bottom: 0px;
+  }
+
+  .main .short-it .short-btn {
+    width: 20%;
+    color: #fff;
+    background-color: hsl(180, 66%, 49%);
+    padding: 10px;
+    margin-left: 20px;
+    border: none;
+    border-radius: 10px;
+    font-weight: 700;
+  }
+
+  .main .shorted {
+    width: 80%;
+    padding: 20px;
+    flex-direction: row;
+    margin-top: 30px;
+    margin-left: 10%;
+  }
+  
+  .main .shorted .origin-url {
+    width: 80%;
+    padding: 8px;
+    border-bottom: none;
+    margin-bottom: 0;
+  }
+
+  .main .shorted .short-url {
+    width: 40%;
+    padding: 8px;
+    color: hsl(180, 66%, 49%);
+    text-align: right;
+    margin-right: 10px;
+  }
+
+  .main .shorted .short-btn {
+    width: 20%;
+    color: #fff;
+    background-color: hsl(180, 66%, 49%);
+    padding: 10px;
+    border: none;
+    border-radius: 10px;
+    font-weight: 700;
+  }
+
+  .main .shorted .copy-btn {
+    display: none;
+    width: 20%;
+    color: #fff;
+    background-color: hsl(255, 11%, 22%);
+    padding: 10px;
+    border: none;
+    border-radius: 10px;
+    font-weight: 700;
+  }
+
+  .about {
+    margin-bottom: 50px;
+  }
+  
+  .features {
+    flex-direction: row;
+    margin-top: 20px;
+    padding-top: 20px;
+  }
+
+
+  .one {
+    margin-top: 70px;
+  }
+
+  .two {
+    margin-top: 170px;
+  }
+
+  .stroke {
+    width: 200px;
+    height: 3px;
+  }
+
+  .footer {
+    padding: 50px 40px;
+    flex-direction: row;
+    align-items: baseline;
+    justify-content: space-between;
+  }
+
+  .footer .footer-feature h3 {
+    margin-bottom: 0px;
+  }
+
+  .resources {
+    margin: 0;
+  }
 }
 </style>
